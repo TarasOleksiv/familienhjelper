c38902949691c2c9602b0d91c03f747573229fb5
Serhii Kostenko <skostenko@ukr.net>
2020-09-04 19:43:12 +0300

    add user admin_test with same as pwd + add it to admin role

--- src/main/resources/database/populateDB.sql
+++ src/main/resources/database/populateDB.sql
@@ -10,12 +10,15 @@ INSERT INTO roles (id, name) VALUES
 INSERT INTO users (id, username, password, email) VALUES
   (uuid_in(md5(random()::text || now()::text)::cstring), 'admin', '$2a$10$MBNLCmt0VanScq0U.glhwulABB06a5z8RIBFOsL4xhoxegaSdNuH6','admin@example.com'),
   (uuid_in(md5(random()::text || now()::text)::cstring), 'toleksiv', '$2a$10$d1dnh071WHfnTzSaHL.T3.loKhECNnmEqwgPMYhTgpwuEbvMSngpu','toleksiv@example.com'),
-  (uuid_in(md5(random()::text || now()::text)::cstring), 'guest', '$2a$10$ODDivyvkir2Oh1RHc5ajb.5Ftv7Q2wUUoqhyojSyxvny8rmPYt03a','guest@example.com')
+  (uuid_in(md5(random()::text || now()::text)::cstring), 'guest', '$2a$10$ODDivyvkir2Oh1RHc5ajb.5Ftv7Q2wUUoqhyojSyxvny8rmPYt03a','guest@example.com'),
+  (uuid_in(md5(random()::text || now()::text)::cstring), 'admin_test', '$2a$10$rJTv8Vgz5bTVPj6xyYiqVeH6ULD..kHEKBFcgluYeuODXNkyhYXxy','admin_test@example.com')
 ;
 
 -- 03. Populate user roles;
 INSERT INTO user_roles (user_id, role_id)
  SELECT u.id AS user_id, r.id AS role_id FROM users u, roles r WHERE u.username = 'admin'  AND r.name = 'ROLE_ADMIN';
+INSERT INTO user_roles (user_id, role_id)
+ SELECT u.id AS user_id, r.id AS role_id FROM users u, roles r WHERE u.username = 'admin_test'  AND r.name = 'ROLE_ADMIN';
 INSERT INTO user_roles (user_id, role_id)
  SELECT u.id AS user_id, r.id AS role_id FROM users u, roles r WHERE u.username = 'guest'  AND r.name = 'ROLE_FU';
 INSERT INTO user_roles (user_id, role_id)
