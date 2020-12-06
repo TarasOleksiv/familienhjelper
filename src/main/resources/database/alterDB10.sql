c3ba2bc74caf7fc0d1c5ab35115c0693c365c158
Taras Oleksiv <toleksiv69@gmail.com>
2020-12-06 20:04:04 +0200

    clean up db scripts

--- src/main/resources/database/alterDB10.sql
+++
@@ -1,7 +0,0 @@
--- 10. Add columns to the table projects
-ALTER TABLE projects
-    ADD COLUMN beneficiary_id UUID;
-
-ALTER TABLE projects
-  ADD CONSTRAINT FK_BENEFICIARY_ID FOREIGN KEY (beneficiary_id) REFERENCES beneficiaries (id) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
--- src/main/resources/database/alterDB9.sql
+++
@@ -1,3 +0,0 @@
--- 05. Increase field size, table projects
-ALTER TABLE projects ALTER COLUMN description TYPE varchar(40000);
-
